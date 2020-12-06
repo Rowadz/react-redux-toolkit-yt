97e9f5eb4da594c431088a4cb191d8f8d0f56028
MohammedAl-rowad <rowadwo@gmail.com>
2020-12-06 20:43:24 +0200

    form

--- src/App.js
+++ src/App.js
@@ -1,58 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from 'react'
+import 'rsuite/dist/styles/rsuite-dark.css'
+import {
+  Button,
+  Container,
+  Header,
+  Navbar,
+  Content,
+  FlexboxGrid,
+  Panel,
+  Form,
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  ButtonToolbar,
+  Divider,
+} from 'rsuite'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="main">
+      <Container>
+        <Header>
+          <Navbar appearance="inverse">
+            <Navbar.Header>
+              <p className="navbar-brand">Redux React TODO</p>
+            </Navbar.Header>
+          </Navbar>
+        </Header>
+        <Content>
+          <FlexboxGrid justify="center">
+            <FlexboxGrid.Item colspan={12}>
+              <Panel header={<h3>Add ToDo</h3>} bordered>
+                <Form fluid>
+                  <FormGroup>
+                    <ControlLabel>What you want to do?</ControlLabel>
+                    <FormControl name="task" />
+                  </FormGroup>
+                  <FormGroup>
+                    <ButtonToolbar>
+                      <Button appearance="primary">Create</Button>
+                    </ButtonToolbar>
+                  </FormGroup>
+                </Form>
+              </Panel>
+              <Divider />
+            </FlexboxGrid.Item>
+          </FlexboxGrid>
+        </Content>
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
